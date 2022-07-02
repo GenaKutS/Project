3f66ef4597d67d305a9a2cdfe064c41cc62b630e
GenaKuts <kutsgena@gmail.com>
2022-07-02 13:25:17 +0300

    get request validation

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
-
+const cors = require ('cors')
+const router = require('./routes')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use('/api', router)
 
 const start = async () => {
     try {
