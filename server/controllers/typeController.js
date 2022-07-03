21c0339f2ba3119cbd7138e4e797ed12447cc028
GenaKuts <kutsgena@gmail.com>
2022-07-03 16:47:09 +0300

    CRUD/Filtering/Pagination

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -9,6 +9,9 @@ class TypeController{
         return res.json(type)
     }
     async getAll(req, res){
+
+        const types = await Type.findAll()
+        return res.json(types)  
     }
  
 }
--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express')
+const deviceController = require('../controllers/deviceController')
 // получаем роут из експрес
 const router = new Router()
+const device = require('../controllers/deviceController')
 
-router.post('/')
-router.get('/')
-router.get('/:id')
+router.post('/', deviceController.create)
+router.get('/', deviceController.getAll )
+router.get('/:id', deviceController.getOne )
 
 module.exports = router
