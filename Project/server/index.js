64c5a627731da2a43e22a997b209f5a969e418be
andry-titoruk <51340921+andry-titoruk@users.noreply.github.com>
2022-07-31 22:47:38 +0300

    Update index.js

--- Project/server/index.js
+++ Project/server/index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
-const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const router = require("./routes");
 const errorHandler = require("./middleware/ErrrorHandlingMiddleware");
@@ -11,7 +10,6 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 //app.use(fileUpload({}))
 app.use(express.static(path.resolve(__dirname, "static")));
