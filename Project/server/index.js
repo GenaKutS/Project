586e7aa22b7dddd0f1e2f999446b2f624ae319ae
GenaKutS <105300946+GenaKutS@users.noreply.github.com>
2022-08-26 02:33:05 +0300

    Update index.js

--- Project/server/index.js
+++ Project/server/index.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use(fileUpload({}));
 app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 
 
 app.use("/api", router);
