8be6be8c6151ad18d799e5d68fca1fc0e1f761cc
andry-titoruk <51340921+andry-titoruk@users.noreply.github.com>
2022-08-30 10:39:08 +0300

    Update deviceController.js

--- Project/server/controllers/deviceController.js
+++ Project/server/controllers/deviceController.js
@@ -7,7 +7,7 @@ const Sequelize = require("sequelize");
 class DeviceController {
   async create(req, res, next) {
     try {
-      var { name, price, brandId, typeId, info } = req.body;
+      let { name, price, brandId, typeId, info } = req.body;
 
       const { img } = req.files;
 
